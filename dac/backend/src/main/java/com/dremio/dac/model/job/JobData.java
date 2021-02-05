5010ef09d10522fd565c2008442256e2bb1bf609
João Pedro <joaop@simbioseventures.com>
2021-02-05 11:23:44 -0300

    docs: add javadoc to range method on JobData class

--- dac/backend/src/main/java/com/dremio/dac/model/job/JobData.java
+++ dac/backend/src/main/java/com/dremio/dac/model/job/JobData.java
@@ -30,18 +30,24 @@ public interface JobData extends AutoCloseable {
   String MAX_CELL_SIZE_KEY = "cellSizeLimit";
 
   /**
-   * Create a data object that contains the results in given range. If the range contains no values, a JobData object
+   * Creates a {@link JobDataFragment} object containing the results in a defined offset and limit row range.
+   * <p>
+   * If the defined range contains no values, an empty JobDataFragment object
    * containing no results is returned.
    *
-   * @param allocator Allocator to accept data
-   * @param offset Number of starting row to include in output
-   * @param limit Max number of rows starting from offset.
-   * @return
+   * @param allocator a buffer allocator to accept the result data
+   * @param offset    the number of the starting row to include in the output result
+   * @param limit     the maximum number of rows to consider in the output result
+   *                  based on the starting offset.
+   * @return the job results defined in a given offset and limit range
+   * @see JobDataFragment
+   * @see JobDataWrapper
    */
   JobDataFragment range(BufferAllocator allocator, int offset, int limit);
 
   /**
    * Create a new data object that truncates the results to at max given rows.
+   *
    * @param allocator Allocator to accept data
    * @param maxRows
    */
