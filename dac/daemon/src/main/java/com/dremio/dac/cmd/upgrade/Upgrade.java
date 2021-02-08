8ebc5ea74cfc26e252bc02151308d99c2796c2d4
Fernanda Nascimento <nandinha.cefet@hotmail.com>
2021-02-08 15:21:52 -0300

    docs: add javadoc to all methods in Upgrade.java

--- dac/daemon/src/main/java/com/dremio/dac/cmd/upgrade/Upgrade.java
+++ dac/daemon/src/main/java/com/dremio/dac/cmd/upgrade/Upgrade.java
@@ -55,7 +55,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
 /**
- * Upgrade command.
+ * Upgrade represents the upgrade command.
  *
  * Extracts store version and uses it to decide if upgrade is possible and which tasks should be executed.
  * If no version is found, the tool assumes it's 1.0.6 as there is no way to identify versions prior to that anyway
@@ -64,6 +64,7 @@ import com.google.common.base.Strings;
  * 2. Repeat task run is it was not successful
  */
 @AdminCommand(value = "upgrade", description = "Upgrades KV store version")
+
 public class Upgrade {
 
   private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Upgrade.class);
@@ -89,6 +90,16 @@ public class Upgrade {
 
   private final List<? extends UpgradeTask> upgradeTasks;
 
+  /**
+   * Constructs an Upgrade object.
+   *  <p>
+   *  Gets an upgrade tasks list, mapping  to task with class with a no-arg constructor and filtering by non abstract class.
+   *  Next, filter out null values and sort upgrade tasks based on dependencies.
+   *
+   * @param dacConfig     a Daemon configuration instance
+   * @param classPathScan a ScanResult object containing all upgrade tasks implementations
+   * @param verbose       a flag which indicates if a log can be displayed
+   */
   public Upgrade(DACConfig dacConfig, ScanResult classPathScan, boolean verbose) {
     this.dacConfig = dacConfig;
     this.classpathScan = classPathScan;
@@ -130,20 +141,46 @@ public class Upgrade {
     this.upgradeTasks = upgradeTaskDependencyResolver.topologicalTasksSort();
   }
 
+  /**
+   * Gets the Daemon configuration instance.
+   *
+   * @return the Daemon configuration instance
+   */
   protected DACConfig getDACConfig() {
     return dacConfig;
   }
 
+  /**
+   * Gets the UpgradeTask implementation.
+   *
+   * @return the UpgradeTask implementation
+   */
   @VisibleForTesting
   List<? extends UpgradeTask> getUpgradeTasks() {
     return upgradeTasks;
   }
 
+  /**
+   * Retrieves a stored version in cluster.
+   * <p>
+   * In case of the version returned of the cluster is null, will be returned the version "1.0.6".
+   *
+   * @param identity a ClusterIdentity instance, to obtain the cluster's information
+   * @return         stored version in cluster
+   */
   static Version retrieveStoreVersion(ClusterIdentity identity) {
     final Version storeVersion = fromClusterVersion(identity.getVersion());
     return storeVersion != null ? storeVersion : LegacyUpgradeTask.VERSION_106;
   }
 
+  /**
+   * Ensures that the current upgrade is supported.
+   * <p>
+   * Verifies if isn't trying makes a downgrade and if the stored version
+   * is "2.0" or higher.
+   *
+   * @param storeVersion the version string to be checked
+   */
   protected void ensureUpgradeSupported(Version storeVersion) {
 
     boolean versionDowngradeCheck = true;
@@ -169,10 +206,21 @@ public class Upgrade {
       "Upgrading from %s to %s is not supported.  Please upgrade to 2.0 first.", storeVersion, VERSION);
   }
 
+  /**
+   * Runs the upgrade command
+   *
+   * @throws Exception If any exceptions or errors occurs
+   */
   public void run() throws Exception {
     run(true);
   }
 
+  /**
+   * Runs the upgrade command.
+   *
+   * @param noDBOpenRetry a flag which indicates if it should retry in case of the database can't open
+   * @throws Exception If any exceptions or errors occurs
+   */
   public void run(boolean noDBOpenRetry) throws Exception {
     Optional<LocalKVStoreProvider> storeOptional = CmdUtils.getKVStoreProvider(dacConfig.getConfig(), classpathScan, noDBOpenRetry);
     if (!storeOptional.isPresent()) {
@@ -188,10 +236,13 @@ public class Upgrade {
   }
 
   /**
-   * If previous edition is available checks if dremio edition we are upgrading to is the same.
-   * @param storeProvider
-   * @param curEdition
-   * @throws Exception
+   * Validates an upgrade.
+   * <p>
+   * Checks if previous edition is available and Dremio edition it is upgrading to is the same.
+   *
+   * @param storeProvider the Provider object that contains the version to be checked if is available
+   * @param curEdition    the Dremio version
+   * @throws Exception If any exception or errors occurs
    */
   @VisibleForTesting
   public void validateUpgrade(final LegacyKVStoreProvider storeProvider, final String curEdition) throws Exception {
@@ -208,6 +259,12 @@ public class Upgrade {
     }
   }
 
+  /**
+   * Runs the upgrade command.
+   *
+   * @param storeProvider the Provider object that contains the version to be checked if is available
+   * @throws Exception If any exception or errors occurs
+   */
   public void run(final LegacyKVStoreProvider storeProvider) throws Exception {
     final Optional<ClusterIdentity> identity =
       BasicSupportService.getClusterIdentity(storeProvider);
@@ -267,10 +324,11 @@ public class Upgrade {
   }
 
   /**
-   * To upgrade with update of UpgradeStore
-   * @param context
-   * @param kvStoreVersion
-   * @throws Exception
+   * Executes the upgrade command to upgrade with update of UpgradeStore.
+   *
+   * @param context         an UpgradeContext instance that contains all stores required by all upgrade tasks
+   * @param kvStoreVersion  the stored version metadata
+   * @throws Exception If any exception or errors occurs
    */
   static void upgradeExternal(UpgradeTask upgradeTask, UpgradeContext context,
                               UpgradeStore upgradeStore, Version kvStoreVersion) throws
@@ -293,13 +351,17 @@ public class Upgrade {
     completeUpgradeTaskRun(upgradeTask, upgradeStore, startTime, UpgradeStatus.COMPLETED);
   }
   /**
-   * To check if max version of the task is less then current KVStore version
+   * Checks if it should proceed with the upgrade.
+   * <p>
+   * Compares if max version of the task is less than current KVStore version
    * if it is true - there is no point of running this task anymore
-   * record it in KVStore, otherwise we will need to proceed with upgrade
-   * @param upgradeStore
-   * @param kvStoreVersion
-   * @return
-   * @throws Exception
+   * record it in KVStore, otherwise we will need to proceed with upgrade.
+   *
+   * @param upgradeTask     an UpgradeTask implementation for all upgrade tasks
+   * @param upgradeStore    a KVStore object to store Upgrade Tasks
+   * @param kvStoreVersion  the stored version
+   * @return a flag which indicates if it should proceed with the upgrade
+   * @throws Exception If any exception or errors occurs
    */
   private static boolean proceedWithUpgrade(UpgradeTask upgradeTask, UpgradeStore upgradeStore, Version kvStoreVersion) throws
     Exception {
@@ -324,12 +386,16 @@ public class Upgrade {
   }
 
   /**
-   * Insert entry into UpgradeStore with task completion
-   * @param upgradeStore
-   * @param startTime
-   * @param upgradeStatus
-   * @return UpgradeTaskSTore object
-   * @throws Exception
+   * Completes the update task run to upgrade with update of UpgradeStore.
+   * <p>
+   * Inserts entry into UpgradeStore with task completion.
+   *
+   * @param upgradeTask   an UpgradeTask implementation for all upgrade tasks
+   * @param upgradeStore  a KVStore object to store Upgrade Tasks
+   * @param startTime     the initial time to the current upgrade task run
+   * @param upgradeStatus the status of the update task
+   * @return              an UpgradeTaskSTore object
+   * @throws Exception If any exception or errors occurs
    */
   private static UpgradeTaskStore completeUpgradeTaskRun(
     UpgradeTask upgradeTask, UpgradeStore upgradeStore, long startTime, UpgradeStatus upgradeStatus)
@@ -341,6 +407,13 @@ public class Upgrade {
     return upgradeStore.addUpgradeRun(upgradeTask.getTaskUUID(), upgradeTask.getTaskName(), upgradeTaskRun);
   }
 
+  /**
+   * Executes an upgrade command.
+   * <p>
+   * Main function to executes the upgrade command.
+   *
+   * @param args a string array with the arguments to execute the main method
+   */
   public static void main(String[] args) {
     final DACConfig dacConfig = DACConfig.newConfig();
     final ScanResult classPathScan = ClassPathScanner.fromPrescan(dacConfig.getConfig().getSabotConfig());
